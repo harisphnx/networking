1fc1548fe583eb5b92b46c2191d7fdd40c9c99aa
Md Haris Iqbal <haris.phnx@gmail.com>
2016-02-02 10:31:44 +0530

    Delete testting1.py

--- router/testting1.py
+++
@@ -1,8 +0,0 @@
-def crc(msg, div, code):
-    msg = list(msg)
-    div = list(div)
-    for i in range(len(msg)-len(code)):
-        if msg[i] == '1':
-            for j in range(len(div)):
-                msg[i+j] = str((int(msg[i+j])+int(div[j]))%2)
-    return ''.join(msg[-len(code):])
